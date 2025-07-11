e18d66bd08bcd7dd67ce02658bb3c6d3d2548a5b
Davichobits <davidruizdeveloper@gmail.com>
2025-07-11 15:20:20 -0500

    feat: enhance GET endpoint for todos with pagination support

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -11,11 +11,11 @@ export async function GET(){
 
   await prisma.todo.createMany({
     data: [
-      {description: 'Piedra del alma', complete: true},
-      {description: 'Piedra del poder'},
-      {description: 'Piedra del tiempo'},
-      {description: 'Piedra del espacio'},
-      {description: 'Piedra del realidad'},
+      {description: '1. Piedra del alma', complete: true},
+      {description: '2. Piedra del poder'},
+      {description: '3. Piedra del tiempo'},
+      {description: '4. Piedra del espacio'},
+      {description: '5. Piedra del realidad'},
 
     ]
   })
--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,9 +1,25 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
-export async function GET(){
+export async function GET(request: Request){
 
-  const todos = await prisma.todo.findMany()
+  const {searchParams} = new URL(request.url);
+
+  const take = searchParams.get('take') ?? '10';
+  const skip = searchParams.get('skip') ?? '0';
+
+  if(isNaN(+take)){
+    return NextResponse.json({message: 'Take tiene que ser un número'}, {status: 400})
+  }
+
+  if(isNaN(+skip)){
+    return NextResponse.json({mesage: 'Skip tiene que ser un número'}, {status:400})
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: Number(take),
+    skip: +skip
+  });
 
   return NextResponse.json(todos);
 }
