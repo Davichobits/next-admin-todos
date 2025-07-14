ea949dd59abea79421c1967d28c95799cae91850
Davichobits <davidruizdeveloper@gmail.com>
2025-07-14 16:21:04 -0500

    fix: change findUnique to findFirst for todo retrieval and ensure consistent error handling

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request, args: Args) {
   const {params} = args;
   const {id} = params;
 
-  const todo = await prisma.todo.findUnique({
+  const todo = await prisma.todo.findFirst({
     where: { id: id }
   })
 
@@ -21,4 +21,27 @@ export async function GET(request: Request, args: Args) {
   }
 
   return NextResponse.json(todo);
+}
+
+export async function PUT(request: Request, args: Args) { 
+
+  const {params} = args;
+  const {id} = params;
+
+  const todo = await prisma.todo.findFirst({
+    where: { id: id }
+  })
+
+  if(todo === null){
+    return NextResponse.json({message: `El id ${id} no existe.`}, {status: 400})
+  }
+
+  const body = await request.json();
+
+  const updatedTodo = await prisma.todo.update({
+    where: {id},
+    data: body
+  })
+
+  return NextResponse.json(updatedTodo);
 }
