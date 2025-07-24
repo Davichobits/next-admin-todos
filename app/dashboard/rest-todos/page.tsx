c2af4eac11ef458c47a58042058d8656c4b081c6
Davichobits <davidruizdeveloper@gmail.com>
2025-07-23 20:46:54 -0500

    Fetching data from server component

--- app/dashboard/rest-todos/page.tsx
+++ app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,33 @@
+// 'use client';
+// import { useEffect } from 'react';
 
-export default function RestTodosPage() {
+import prisma from '@/lib/prisma';
+
+export const metadata = {
+  title: 'Listado de todos',
+  description: 'Todos los To do'
+}
+
+export default async function RestTodosPage() {
+
+  const todos = await prisma.todo.findMany({
+    orderBy: {
+      description: 'asc'
+    }
+  })
+
+  // useEffect(()=>{
+  //   fetch('/api/todos')
+  //     .then(resp => resp.json())
+  //     .then(data => console.log(data))
+  // },[])
+
   return (
     <div>
       <h1>Rest Todos Page</h1>
+      {
+        todos.map(todo => <p key={todo.id}>{todo.description}</p>)
+      }
     </div>
   );
 }
